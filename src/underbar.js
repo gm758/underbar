dbe1add48847d3c2e29b9aae5b8abae73bb3f7c4
Griffin Michl <griffinmichl@gmail.com>
2015-11-14 18:02:11 +0700

    map should return an array regardless of input

--- src/underbar.js
+++ src/underbar.js
@@ -106,15 +106,14 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var result = [];
     if (Array.isArray(collection)) {
-      var result = [];
       for (var i = 0; i < collection.length; i++) {
         result.push(iterator(collection[i], i, collection));
       }
     } else {
-      var result = {};
       for (var elem in collection) {
-        result[elem] = iterator(collection[i], i, collection);
+        result.push(iterator(collection[elem], elem, collection);
       }
     }
     return result;
